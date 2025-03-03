d851ccdc3565b1bf4dd175c8d59f8be890d450ed
hoeller9 <hoeller.marco9@gmail.com>
2025-03-03 20:58:55 +0100

    created first project structure

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          TimeTrack
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/history" label="Historie" />
+        <q-route-tab to="/tracking" label="Aufzeichnungen" />
+        <q-route-tab to="/personalInformation" label="Personendaten" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<style scoped>
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,6 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <h1>TEST</h1>
   </q-page>
 </template>
 
